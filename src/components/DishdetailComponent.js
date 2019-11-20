2842799cb5334219f574689bc31f444f8e55437c
Lisandro <lisandrozanni@hotmail.com>
2019-11-20 07:43:47 -0300

    Presentational and Container Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -10,30 +10,34 @@ import {
 class DishDetail extends Component {
   renderDish(dish) {
     return (
-      <Card>
-        <CardImg top src={dish.image} alt={dish.name} />
-        <CardBody>
-          <CardTitle>{dish.name}</CardTitle>
-          <CardText>{dish.description}</CardText>
-        </CardBody>
-      </Card>
+      <div className="col-12 col-md-5 m-1">
+        <Card>
+          <CardImg top src={dish.image} alt={dish.name} />
+          <CardBody>
+            <CardTitle>{dish.name}</CardTitle>
+            <CardText>{dish.description}</CardText>
+          </CardBody>
+        </Card>
+      </div>
     );
   }
 
   renderComments(comments) {
     return (
-      <ul className="list-unstyled">
-        <h4>{'Comments'}</h4>
-          {comments.map(comment => {
-            return (
-              <li key={comment.id}>
-                <p>{comment.comment}</p>
-                <p>{'-- ' + comment.author + ', '}  
-                {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-              </li>
-            );
-          })}
-      </ul>
+      <div className="col-12 col-md-5 m-1">
+        <ul className="list-unstyled">
+          <h4>{'Comments'}</h4>
+            {comments.map(comment => {
+              return (
+                <li key={comment.id}>
+                  <p>{comment.comment}</p>
+                  <p>{'-- ' + comment.author + ', '}  
+                  {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                </li>
+              );
+            })}
+        </ul>
+      </div>
     );
   }
 
@@ -42,12 +46,8 @@ class DishDetail extends Component {
       return (
         <div className="container">
           <div className="row">
-            <div className="col-12 col-md-5 m-1">
-              {this.renderDish(this.props.dish)}
-            </div>
-            <div className="col-12 col-md-5 m-1">
-              {this.renderComments(this.props.dish.comments)}
-            </div>
+            {this.renderDish(this.props.dish)}
+            {this.renderComments(this.props.dish.comments)}
           </div>
         </div>
       );
